4695c4027ed5f8f48fb5f238638f4363f0ea1207
Paul Spende <paul.spende@gmail.com>
2020-04-19 03:07:26 +0200

    fixed cards style

--- components/CardsItem.js
+++ components/CardsItem.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Card, Paragraph } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export const CardsItem = ({ question }) => {
   return (
-    <Card style={styles.card}>
-      <Card.Content>
-        <Paragraph>{question}</Paragraph>
-      </Card.Content>
-    </Card>
+    <View style={styles.cardWrapper}>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Paragraph>{question}</Paragraph>
+        </Card.Content>
+      </Card>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  card: {
+  cardWrapper: {
     flexGrow: 0.5,
     flexBasis: 0.5,
-    flexShrink: 0,
-
+  },
+  card: {
     height: 200,
     margin: 8,
 
