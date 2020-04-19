b52936fadca121a8887565c5467c4f2a9c69deb9
Paul Spende <paul.spende@gmail.com>
2020-04-19 02:39:39 +0200

    added provider and themes

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Provider as PaperProvider, DefaultTheme, configureFonts } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
+import { createStore } from 'redux';
 
 import reducer from './reducers';
-import { DeckList } from './components/DeckList';
+import { Decks } from './Views/Decks';
+import { Cards } from './Views/Cards';
 
-export default function App() {
-  return (
-    <Provider store={createStore(reducer)}>
-      <View style={styles.container}>
-        <DeckList />
-      </View>
-    </Provider>
-  );
+const fontConfig = {
+  default: {
+    regular: {
+      fontFamily: 'Roboto',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'Roboto',
+      fontWeight: '500',
+    },
+    light: {
+      fontFamily: 'Roboto',
+      fontWeight: '300',
+    },
+    thin: {
+      fontFamily: 'Roboto',
+      fontWeight: '100',
+    },
+  },
+};
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 8,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#FFF',
+    accent: '#000',
+  },
+  fonts: configureFonts(fontConfig),
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <StoreProvider store={createStore(reducer)}>
+        <PaperProvider theme={theme}>
+          <View style={styles.container}>
+            <Cards />
+          </View>
+        </PaperProvider>
+      </StoreProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
