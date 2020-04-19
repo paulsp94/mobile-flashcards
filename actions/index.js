1ffdac952e1857db16d73f57846afcc9e90d6d40
Paul Spende <paul.spende@gmail.com>
2020-04-19 22:21:39 +0200

    added new actions

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,10 @@
 export const GET_DECKS = 'GET_DECKS';
 export const GET_DECK = 'GET_DECK';
 export const ADD_DECK_TITLE = 'ADD_DECK_TITLE';
+export const EDIT_DECK_TITLE = 'EDIT_DECK_TITLE';
+export const REMOVE_DECK_TITLE = 'REMOVE_DECK_TITLE';
 export const ADD_QUESTION = 'ADD_QUESTION';
+export const REMOVE_QUESTION = 'REMOVE_QUESTION';
 
 export const getDecks = () => ({
   type: GET_DECKS,
@@ -17,8 +20,25 @@ export const addDeckTitle = (title) => ({
   title,
 });
 
-export const addQuestion = (deckName, question) => ({
+export const editDeckTitle = (oldTitle, newTitle) => ({
+  type: EDIT_DECK_TITLE,
+  oldTitle,
+  newTitle,
+});
+
+export const removeDeckTitle = (title) => ({
+  type: REMOVE_DECK_TITLE,
+  title,
+});
+
+export const addQuestion = (deck, question) => ({
   type: ADD_QUESTION,
+  deck,
+  question,
+});
+
+export const removeQuestion = (deckName, question) => ({
+  type: REMOVE_QUESTION,
   deckName,
   question,
 });
