211339664726b83b0640afe64709d60cc7f4e0ef
Paul Spende <paul.spende@gmail.com>
2020-04-19 22:23:21 +0200

    added dummy quiz view

--- App.js
+++ App.js
@@ -4,33 +4,44 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
-import { createStore } from 'redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { theme } from './utils/theme';
-import reducer from './reducers';
+import { store, persistor } from './utils/configureStore';
+
 import { Decks } from './Views/Decks';
 import { Cards } from './Views/Cards';
+import { Quiz } from './Views/Quiz';
 
 const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
     return (
-      <StoreProvider store={createStore(reducer)}>
-        <PaperProvider theme={theme}>
-          <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen name="Decks" component={Decks} />
-              <Stack.Screen
-                name="Cards"
-                component={Cards}
-                options={({ route }) => ({
-                  title: route.params.deckName,
-                })}
-              />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </PaperProvider>
+      <StoreProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <PaperProvider theme={theme}>
+            <NavigationContainer>
+              <Stack.Navigator>
+                <Stack.Screen name="Decks" component={Decks} />
+                <Stack.Screen
+                  name="Cards"
+                  component={Cards}
+                  options={({ route }) => ({
+                    title: `${route.params.deckName} (${route.params.cards})`,
+                  })}
+                />
+                <Stack.Screen
+                  name="Quiz"
+                  component={Quiz}
+                  options={({ route }) => ({
+                    title: `${route.params.deckName} (${route.params.cards})`,
+                  })}
+                />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </PaperProvider>
+        </PersistGate>
       </StoreProvider>
     );
   }
---
+++ Views/Quiz.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Container } from '../components/Container';
+
+export const Quiz = ({ navigate }) => {
+  return <Container></Container>;
+};
