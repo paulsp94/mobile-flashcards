87fa71ad11b28cc1780234d4805ec9e420e2d882
Paul Spende <paul.spende@gmail.com>
2020-04-19 02:38:34 +0200

    basic Container

---
+++ components/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export const Container = ({ children }) => {
+  return <View style={styles.container}>{children}</View>;
+};
+
+const styles = StyleSheet.create({ container: { flex: 1, alignSelf: 'stretch' } });
