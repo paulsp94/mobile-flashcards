43a715e87b5ea07f22523f667fb70af76b177f52
Paul Spende <paul.spende@gmail.com>
2020-04-19 22:19:39 +0200

    added uuids

--- reducers/index.js
+++ reducers/index.js
@@ -1,26 +1,38 @@
-import { ADD_DECK_TITLE, ADD_QUESTION } from '../actions';
+import UUIDGenerator from 'react-native-uuid-generator';
+
+import { ADD_DECK_TITLE, ADD_QUESTION, REMOVE_DECK_TITLE, EDIT_DECK_TITLE } from '../actions';
+
+Object.removeTitle = (obj, title) =>
+  Object.keys(obj)
+    .filter((key) => key !== title)
+    .reduce((res, key) => ((res[key] = obj[key]), res), {});
 
 const initialState = {
   React: {
     title: 'React',
+    uuid: 'test1',
     questions: [
       {
         question: 'What is React?',
         answer: 'A library for managing user interfaces',
+        uuid: 'test11',
       },
       {
         question: 'Where do you make Ajax requests in React?',
         answer: 'The componentDidMount lifecycle event',
+        uuid: 'test12',
       },
     ],
   },
   JavaScript: {
     title: 'JavaScript',
+    uuid: 'test2',
     questions: [
       {
         question: 'What is a closure?',
         answer:
           'The combination of a function and the lexical environment within which that function was declared.',
+        uuid: 'test21',
       },
     ],
   },
@@ -29,11 +41,27 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_DECK_TITLE:
-      return { ...state, [action.title]: { title: action.title, questions: [] } };
+      return {
+        ...state,
+        [action.title]: { title: action.title, uuid: UUIDGenerator.getRandomUUID(), questions: [] },
+      };
+    case EDIT_DECK_TITLE: {
+      const newDeck = state;
+      newDeck[action.newTitle] = newDeck[action.oldTitle];
+      return Object.removeTitle(newDeck, action.title);
+    }
+    case REMOVE_DECK_TITLE:
+      return Object.removeTitle(state, action.title);
     case ADD_QUESTION:
       return {
         ...state,
-        [action.deckName]: { questions: [...state[action.deckName].questions, action.question] },
+        [action.deck]: {
+          ...state[action.deck],
+          questions: [
+            ...state[action.deck].questions,
+            { ...action.question, uuid: UUIDGenerator.getRandomUUID() },
+          ],
+        },
       };
     default:
       return state;
