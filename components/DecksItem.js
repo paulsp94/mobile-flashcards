1ae4b1bed66e1fe39b92ab3f8500f2fcbffe0655
Paul Spende <paul.spende@gmail.com>
2020-04-19 22:21:28 +0200

    added delete functionality

--- components/DecksItem.js
+++ components/DecksItem.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { Card, IconButton } from 'react-native-paper';
+import React, { useState } from 'react';
+import { Card, IconButton, Menu } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { removeDeckTitle } from '../actions';
 
-const RightContent = () => (
-  <IconButton icon="dots-vertical" color="black" onPress={() => console.log('Pressed')} />
-);
+export const DecksItem = ({ deckName, cards, navigate, showModal }) => {
+  const [visible, setVisible] = useState(false);
+  const dispatch = useDispatch();
 
-export const DecksItem = ({ deckName, cards, navigate }) => (
-  <Card style={styles.card} onPress={() => navigate(deckName)}>
-    <Card.Title
-      title={deckName}
-      subtitle={`${cards} ${cards === 1 ? 'Card' : 'Cards'}`}
-      right={RightContent}
-      rightStyle={styles.rightStyle}
-    />
-  </Card>
-);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  const toggleDelete = () => dispatch(removeDeckTitle(deckName));
+  // const toggleEdit = () => showModal({ title: deckName });
+
+  return (
+    <Card style={styles.card} onPress={() => navigate(deckName, cards)}>
+      <Card.Title
+        title={deckName}
+        subtitle={`${cards} ${cards === 1 ? 'Card' : 'Cards'}`}
+        right={() => (
+          <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={<IconButton icon="dots-vertical" color="black" onPress={openMenu} />}>
+            {/* <Menu.Item onPress={toggleEdit} title="Edit" /> */}
+            <Menu.Item onPress={toggleDelete} title="Delete" />
+          </Menu>
+        )}
+        rightStyle={styles.rightStyle}
+      />
+    </Card>
+  );
+};
 
 const styles = StyleSheet.create({
   card: {
@@ -30,6 +47,6 @@ const styles = StyleSheet.create({
   rightStyle: {
     position: 'absolute',
     top: 4,
-    right: 0,
+    right: 4,
   },
 });
