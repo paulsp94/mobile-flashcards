21437c5d4667a0d41c4803f4b201e904ce890cd2
Paul Spende <paul.spende@gmail.com>
2020-04-19 03:08:10 +0200

    connected to redux and added navigation

--- Views/Decks.js
+++ Views/Decks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { FlatList, StyleSheet } from 'react-native';
 import { FAB } from 'react-native-paper';
 
@@ -6,62 +7,18 @@ import { Container } from '../components/Container';
 import { DecksItem } from '../components/DecksItem';
 import { CustomAppbar } from '../components/CustomAppbar';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-    cards: 24,
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-    cards: 48,
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-    cards: 36,
-  },
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba2',
-    title: 'First Item',
-    cards: 24,
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f632',
-    title: 'Second Item',
-    cards: 48,
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d722',
-    title: 'Third Item',
-    cards: 36,
-  },
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba3',
-    title: 'First Item',
-    cards: 24,
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f633',
-    title: 'Second Item',
-    cards: 48,
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d723',
-    title: 'Third Item',
-    cards: 36,
-  },
-];
+export const Decks = ({ navigation }) => {
+  const decks = useSelector((state) =>
+    Object.values(state).map((deck) => ({ title: deck.title, cards: deck.questions.length }))
+  );
 
-export const Decks = () => {
   return (
     <Container>
       <CustomAppbar />
       <FlatList
-        data={DATA}
+        data={decks}
         renderItem={({ item }) => <DecksItem deckName={item.title} cards={item.cards} />}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.title}
         style={styles.list}
         contentContainerStyle={styles.listContainer}
       />
