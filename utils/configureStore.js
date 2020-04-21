5648716fa2ecc0e3c2c106d9e9bf95389b59743d
Paul Spende <paul.spende@gmail.com>
2020-04-21 18:57:00 +0200

    fixed cleanup

--- utils/configureStore.js
+++ utils/configureStore.js
@@ -3,12 +3,12 @@ import { persistStore, persistReducer } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
 import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 
-import { decks, quiz } from '../reducers';
+import { decks } from '../reducers';
 
 const rootPersistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  blacklist: ['decks', 'quiz'],
+  blacklist: ['decks'],
 };
 
 const decksPersistConfig = {
@@ -17,20 +17,11 @@ const decksPersistConfig = {
   stateReconciler: autoMergeLevel2,
 };
 
-const quizPersistConfig = {
-  key: 'quiz',
-  storage: AsyncStorage,
-};
-
 const rootReducer = combineReducers({
   decks: persistReducer(decksPersistConfig, decks),
-  quiz: persistReducer(quizPersistConfig, quiz),
 });
 
 const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
-export const store = createStore(
-  persistedReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = createStore(persistedReducer);
 export const persistor = persistStore(store);
