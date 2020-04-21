fb20f8d44ec8e1237839b3c7cae3af14af598667
Paul Spende <paul.spende@gmail.com>
2020-04-21 20:56:12 +0200

    scroll for questions

--- components/cards/CardsItem.js
+++ components/cards/CardsItem.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { Card, Title } from 'react-native-paper';
-import { StyleSheet, View } from 'react-native';
+import { Card } from 'react-native-paper';
+import { StyleSheet, View, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export const CardsItem = ({ question }) => {
   return (
     <View style={styles.cardWrapper}>
       <Card style={styles.card}>
-        <Card.Content>
-          <Title>{question}</Title>
+        <Card.Content style={styles.cardContent}>
+          <LinearGradient
+            colors={['white', 'transparent']}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 16,
+              height: 16,
+              zIndex: 10,
+            }}
+          />
+          <ScrollView>
+            <Text style={{ fontSize: 24, lineHeight: 32 }}>{question}</Text>
+          </ScrollView>
+          <LinearGradient
+            colors={['transparent', 'white']}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 16,
+              height: 16,
+              zIndex: 10,
+            }}
+          />
         </Card.Content>
       </Card>
     </View>
@@ -26,5 +52,8 @@ const styles = StyleSheet.create({
     margin: 8,
 
     elevation: 10,
+  },
+  cardContent: {
+    flex: 1,
   },
 });
